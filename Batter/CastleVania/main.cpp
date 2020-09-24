cd3cc634c208aba1d8e8deb6aba69a44dd0b65c4
DESKTOP-OIEU4AK\95doanphuong <95doanphuong@gmail.com>
2020-09-24 22:04:51 +0700

    init project

---
+++ Batter/CastleVania/main.cpp
@@ -0,0 +1,11 @@
+#include <windows.h>
+
+int WINAPI WinMain(
+	HINSTANCE hInstance, 
+	HINSTANCE hPrevInstance, 
+	LPSTR lpCmdLine, 
+	int nCmdShow)
+{
+	MessageBox(0, "Welcome to Game Programming!", "Hello DirectX", MB_OK);
+	return 0;
+}
